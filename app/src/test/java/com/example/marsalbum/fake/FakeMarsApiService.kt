7d8b731eee0a2fcd4734fc62dfe6746fa80d5e62
KingsMunene <munenekings2@gmail.com>
2024-01-12 09:36:54 +0300

    Add fake MarsApiService that provides the fake data to the networkrepository implementation

--- app/src/test/java/com/example/marsalbum/fake/FakeMarsApiService.kt
+++ app/src/test/java/com/example/marsalbum/fake/FakeMarsApiService.kt
@@ -1,4 +1,9 @@
 package com.example.marsalbum.fake
 
-interface FakeMarsApiService {
+import com.example.marsalbum.network.MarsAlbumApiService
+import com.example.marsalbum.network.MarsPhoto
+
+class FakeMarsApiService : MarsAlbumApiService {
+    override suspend fun getPhotos(): List<MarsPhoto> = FakeDataSource.photosList
+
 }
